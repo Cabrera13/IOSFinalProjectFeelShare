889d7b1701ef47de56d2c9ac2b9d27e73cb106f0
ios <ios@mc-0019.uvic.local>
2018-05-25 19:06:47 +0200

    file deleted and updated

---
+++ ListFirebase/RegisterScreen.swift
@@ -0,0 +1,65 @@
+//
+//  RegisterScreen.swift
+//  ListFirebase
+//
+//  Created by ios on 25/05/18.
+//  Copyright © 2018 ios. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+class RegisterScreen: UIViewController {
+
+    @IBOutlet weak var buttonRegister: UIButton!
+    @IBOutlet weak var textFieldPassword: UITextField!
+    @IBOutlet weak var textFieldUsername: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func buttonReg(_ sender: Any) {
+        if textFieldUsername.text == "" {
+            let alertController = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+        }
+        else {
+            Auth.auth().createUser(withEmail: textFieldUsername.text!, password: textFieldPassword.text!) { (user, error) in
+                
+                if error == nil {
+                    print("You have successfully signed up")
+                    
+                    
+                } else {
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ListFirebase/ViewController.swift
+++ ListFirebase/ViewController.swift
@@ -7,42 +7,13 @@
 //
 
 import UIKit
-import FirebaseAuth
+
 class ViewController: UIViewController {
 
-    @IBOutlet weak var textFieldPassword: UITextField!
-    @IBOutlet weak var textFieldUsername: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
-    @IBAction func buttonRegister(_ sender: Any) {
-        if textFieldUsername.text == "" {
-            let alertController = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
-            
-            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-            alertController.addAction(defaultAction)
-            
-            present(alertController, animated: true, completion: nil)
-        }
-        else {
-            Auth.auth().createUser(withEmail: textFieldUsername.text!, password: textFieldPassword.text!) { (user, error) in
-                
-                if error == nil {
-                    print("You have successfully signed up")
-                    
-                    
-                } else {
-                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
-                    
-                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-                    alertController.addAction(defaultAction)
-                    
-                    self.present(alertController, animated: true, completion: nil)
-                }
-            }
-        }
     }
 
     override func didReceiveMemoryWarning() {
